bccba2f46365b072de8df80733740920cfac8247
yllmis <2943246638@qq.com>
2025-10-30 20:24:16 +0800

    更新：客户端新增私聊模式

--- client.go
+++ client.go
@@ -99,6 +99,45 @@ func (client *Client) PublicChat() {
 	fmt.Println(">>>>>退出公聊模式...")
 }
 
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUsers()
+
+	fmt.Println(">>>>>请输入聊天对象的用户名，exit退出:")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println(">>>>>请输入消息内容，exit退出:")
+		fmt.Scanln(&chatMsg)
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn.Write err:", err)
+					break
+				}
+			}
+			fmt.Println(">>>>>请输入消息内容，exit退出:")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println(">>>>>请输入聊天对象的用户名，exit退出:")
+		fmt.Scanln(&remoteName)
+	}
+}
+
 func (client *Client) Run() {
 	for client.flag != 0 {
 		for client.menu() != true {
@@ -109,7 +148,7 @@ func (client *Client) Run() {
 			client.PublicChat()
 			break
 		case 2:
-			fmt.Println("私聊模式选择...")
+			client.PrivateChat()
 			break
 		case 3:
 			client.UpdateName()
